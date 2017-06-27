b62ca382c2c2e0b27a428d9afb0c157399740c34
mbialeckiz <marcinbialeckiz@gmail.com>
2017-06-27 11:04:28 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function Phone(brand, price, color) {
+  this.brand = brand;
+  this.price = price; 
+  this.color = color;
+}
+
+Phone.prototype.printInfo = function() {
+  console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+}
+
+var iPhone6S = new Phone("Apple", 2250, "silver");
+var SamsungGalaxyS6 = new Phone("Samsung", 1999, "white");
+var OnePlusOne = new Phone("OnePlus", 1499, "black");
+
+iPhone6S.printInfo();
+SamsungGalaxyS6.printInfo();
+OnePlusOne.printInfo();
